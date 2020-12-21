6d082b60f96d8e917e2924b41fa939cc5aa57303
cloxnu <o18089541818@outlook.com>
2020-12-22 01:22:08 +0800

    Update GST_in_paper.py

--- Summary/其他/GST_in_paper.py
+++ Summary/其他/GST_in_paper.py
@@ -1,5 +1,6 @@
 from itertools import accumulate
 
+
 class State:
     def __init__(self, left=None, right=None, suffix_link=None):
         self.left = left
@@ -9,24 +10,38 @@ class State:
 
 
 class STree:
-    def __init__(self, string):
-        self.string = string
+    def __init__(self):
+        self.string = ""
+        self.end_idxes = []
         self.auxiliary = State()  # the auxiliary state ⊥
         self.root = State(-1, -1, self.auxiliary)
-        for char in string:
+
+    def build(self, string, end_idxes=None):
+        self.string = string
+        self.end_idxes = end_idxes if end_idxes else []
+        self.auxiliary = State()  # the auxiliary state ⊥
+        self.root = State(-1, -1, self.auxiliary)
+        for char in self.string:
             self.auxiliary.transition[char] = self.root
         active_state = self.root
         left = 0
         for idx in range(len(self.string)):
             active_state, left = self._update(active_state, left, idx)
             active_state, left = self._canonize(active_state, left, idx)
+        if self.end_idxes:
+            self.root.transition['end'] = State(self.end_idxes[-1], float("inf"))
+
+    def build_with_automatic_end(self, strings):
+        string = '$'.join(strings) + '$'
+        end_idxes = list(accumulate(strings, lambda x, y: x + len(y) + 1, initial=-1))[1:]
+        self.build(string, end_idxes)
 
     # 打印后缀树
     def __repr__(self):
         def state_desc(state: State):
             if state.left is None: return "⊥"
             if state.left == -1: return self.string
-            return self.string[state.left:] if state.right == float('inf') else self.string[state.left:state.right+1]
+            return (self.string[state.left:] if state.right == float('inf') else self.string[state.left:state.right+1]) + (" (end)" if state.right == float("inf") else "")
 
         def suffix_link_desc(state: State, prefix=""):
             return prefix + state_desc(state.suffix_link) if state.suffix_link else ""
@@ -44,7 +59,7 @@ class STree:
         is_end_point, split_state = self._test_and_split(active_state, left, idx - 1, self.string[idx])
         while not is_end_point:
             new_state = State(idx, float('inf'))
-            split_state.transition[self.string[idx]] = new_state
+            split_state.transition[self.string[idx] if idx not in self.end_idxes else 'end'] = new_state
             if old_active_point != self.root:
                 old_active_point.suffix_link = split_state
             old_active_point = split_state
@@ -59,7 +74,7 @@ class STree:
             return char in active_state.transition, active_state
         next_state = active_state.transition[self.string[left]]
         next_char_idx = next_state.left + right - left + 1
-        if char == self.string[next_char_idx]:
+        if right + 1 not in self.end_idxes and char == self.string[next_char_idx]:
             return True, active_state
         split_state = State(next_state.left, next_char_idx - 1)
         active_state.transition[self.string[left]] = split_state
@@ -84,8 +99,9 @@ class Application:
     # 最长公共子串
     @staticmethod
     def lcs(string1, string2, debug=False):
-        string = string1 + "#" + string2 + "$"
-        tree = STree(string)
+        string = string1 + "$" + string2 + "$"
+        tree = STree()
+        tree.build_with_automatic_end([string1, string2])
         if debug: print(tree)
         
         def dfs(state: State):
@@ -110,36 +126,38 @@ class Application:
     def lcss(strings, debug=False):
         string = '$'.join(strings) + '$'
         str_lens = list(accumulate(strings, lambda x, y: x + len(y) + 1, initial=0))
-        tree = STree(string)
+        tree.build_with_automatic_end(strings)
         if debug: print(tree)
         
         def dfs(state: State):
             if state.right == float('inf'):
-                return '', next(i for i in range(len(str_lens)-1) if state.left < str_lens[i+1])
+                return '', {next(i for i in range(len(str_lens) - 1) if state.left < str_lens[i + 1])}
             res, string_set = [], set()
             for s in state.transition.values():
-                res_string, string_idx = dfs(s)
+                res_string, string_idxes = dfs(s)
                 if len(res_string) > 0:
                     res.append((string[state.left:state.right+1] if state.left != -1 else "") + res_string)
                 else:
-                    string_set.add(string_idx)
+                    string_set.update(string_idxes)
             if len(string_set) == len(strings):
                 res.append(string[state.left:state.right+1])
-            return max(res, key=lambda x: len(x)) if res else '', string_idx
+            return max(res, key=lambda x: len(x)) if res else '', string_set
 
         return dfs(tree.root)[0]
                 
 
-
 if __name__ == '__main__':
-    # tree = STree("abacdacdacdbc$")
-    # tree = STree("cacaocac#ccaooc$")
-    # tree = STree("1412412331$$")
-    # print(tree)
+    tree = STree()
+    # tree = STree("asjknx$")
+    tree.build_with_automatic_end(["abacdacdacdbc"])
+    # tree.build_with_automatic_end(["cacaocac", "ccaooc"])
+    # tree.build("1234332214$")
+    # tree.build("asjknx")
+    print(tree)
 
     # print(Application.lcs("12335665464566321", "12366546456653321"))
-    print(Application.lcs("abcd", "bcd", debug=True))
-    # print(Application.lcss(["abcdfd", "bcdsf"], debug=True))
+    # print(Application.lcs("abcd", "bcd", debug=True))
+    # print(Application.lcss(["abcdfds", "bfdbcdfew", "bcdrgde"], debug=True))
 
     
 
